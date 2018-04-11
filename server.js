5648d22b6cbecdbbf170f2fb47d7253576bc451c
Benjamin Cash <cash.benj@gmail.com>
2018-04-11 16:23:02 -0600

    configuring app for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const db = require("./models")
 
-const PORT = 3000;
+const port = process.env.PORT || 3000
 const app = express();
 
 const axios = require("axios");
